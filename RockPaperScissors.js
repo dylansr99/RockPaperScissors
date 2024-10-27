8f7f751652619a3995e15c117575f0af951f78d4
dylansr99 <dylansr99@gmail.com>
2024-10-27 11:53:54 +0000

    Assign function name, generate random number to be assigned

---
+++ RockPaperScissors.js
@@ -0,0 +1,20 @@
+function getComputerChoice() {
+// Create variable to store randomly generated number between zero and two called ranNumb
+    let ranNumb = Math.floor(Math.random() * 3);
+// When the value in ranNumb is 0 return rock
+      
+
+// When the value in ranNumb is 1 return paper
+
+
+// When the value in ranNumb is 2 return scissors
+
+
+// Create variable for storing returned value 
+
+
+// Print returned value
+    console.log(ranNumb);
+}
+
+getComputerChoice()
