40f44ecea5466c7a5ed79dbe20161c134594aab2
dylansr99 <dylansr99@gmail.com>
2024-10-27 12:24:28 +0000

    Remove pseudocode, tidy code

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,24 +1,9 @@
 function getComputerChoice() {
-// Create variable to store randomly generated number between zero and two called ranNumb
     let ranNumb = Math.floor(Math.random() * 3);
-// Create variable for storing returned value 
     let returnVal = "";
-// When the value in ranNumb is 0 return rock
-      if (ranNumb == 0) {
-           returnVal = "rock"
-      };
-// When the value in ranNumb is 1 return paper
-      if (ranNumb == 1) {
-           returnVal = "paper"
-      };
-
-// When the value in ranNumb is 2 return scissors
-       if (ranNumb == 2) {
-            returnVal = "scissors"
-      };
-
-// Print returned value
+      if (ranNumb == 0) {returnVal = "rock"};
+       if (ranNumb == 1) {returnVal = "paper"};
+        if (ranNumb == 2) {returnVal = "scissors"};
     console.log(returnVal);
 }
-
 getComputerChoice()
