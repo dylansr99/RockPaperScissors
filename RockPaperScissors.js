5d39a303cb1a1c12e6428d85f2c61b86e1df532a
dylansr99 <dylansr99@gmail.com>
2025-01-08 11:44:23 +0000

    Add queryselectors for buttons

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -11,6 +11,9 @@ function playGame() {
 let humanScore = 0;
 let computerScore = 0;
 
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
 
 function playRound(humanChoice, computerChoice) {
   console.log(humanChoice)
