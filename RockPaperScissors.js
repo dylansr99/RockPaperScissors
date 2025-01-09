e455a6b24aaf47a8f9008d8fe96e67aa137f7f64
dylansr99 <dylansr99@gmail.com>
2025-01-09 10:45:03 +0000

    Reenter playRound function after last commit

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -7,6 +7,36 @@ function getComputerChoice() {
         return returnVal;
 }
 
+function playRound(humanChoice, computerChoice) {
+   console.log(humanChoice)
+   console.log(computerChoice)
+   let result = "";
+   let output = "";
+ 
+ 
+   if (humanChoice == "rock" && computerChoice == "paper") {result = "computerWin"}
+    else if (humanChoice == "scissors" && computerChoice == "rock") {result = "computerWin"}
+     else if (humanChoice == "paper" && computerChoice == "scissors") {result = "computerWin"}
+      else if (humanChoice == "rock" && computerChoice == "scissors") {result = "humanWin"}
+       else if (humanChoice == "scissors" && computerChoice == "paper") {result = "humanWin"}
+        else if (humanChoice == "paper" && computerChoice == "rock") {result = "humanWin"}
+         else if (humanChoice == "rock" && computerChoice == "rock") {result = "draw"}
+          else if (humanChoice == "paper" && computerChoice == "paper") {result = "draw"}
+           else if (humanChoice == "scissors" && computerChoice == "scissors") {result = "draw"}
+ 
+   if (result == "computerWin") {output = `You lost! ${computerChoice} beats ${humanChoice}`}
+    else if (result == "humanWin") {output = `You won! ${humanChoice} beats ${computerChoice}`}
+     else if (result == "draw") {output = `Draw! ${humanChoice} cancels out ${computerChoice}`}
+ 
+   console.log(output)
+ 
+   if (result == "computerWin") {computerScore++}
+    else if (result == "humanWin") {humanScore++}
+ 
+   console.log(`Your score is: ${humanScore}.`)
+   console.log(`The computer score is: ${computerScore}`)
+ }
+
 let humanScore = 0;
 let computerScore = 0;
 
